c4fcfafa1539882ac9e68a954187e39bcd04175e
Stevie Hetelekides <hetelek@gmail.com>
2015-08-13 17:59:09 -0400

    Add option to ignore prefixes

--- Model/FolderMigrator.h
+++ Model/FolderMigrator.h
@@ -2,8 +2,11 @@
 @property NSArray *sourceFolderPaths;
 @property NSArray *destinationFolderPaths;
 @property NSArray *ignoreSuffixes;
+@property NSArray *ignorePrefixes;
 
++ (BOOL)migrateDirectory:(NSString *)sourceDirectory toDirectory:(NSString *)destinationDirectory ignorePrefixes:(NSArray *)ignorePrefixes ignoreSuffixes:(NSArray *)ignoreSuffixes;
 + (BOOL)migrateDirectory:(NSString *)sourceDirectory toDirectory:(NSString *)destinationDirectory ignoreSuffixes:(NSArray *)ignoreSuffixes;
++ (BOOL)migrateDirectory:(NSString *)sourceDirectory toDirectory:(NSString *)destinationDirectory ignorePrefixes:(NSArray *)ignorePrefixes;
 
 - (instancetype)initWithSourcePath:(NSString *)sourceFolderPath destinationPath:(NSString *)destinationFolderPath;
 - (BOOL)executeMigration;
