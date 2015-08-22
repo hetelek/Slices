566d0bb5fb609489a8c2b788457f808aed7d6556
Stevie Hetelekides <hetelek@gmail.com>
2015-08-21 22:19:48 -0400

    Add share heart in preferences

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-THEOS_DEVICE_IP = 192.168.1.17 #iphone6+
+#THEOS_DEVICE_IP = 192.168.1.17 #iphone6+
 #THEOS_DEVICE_IP = 192.168.1.5 #iphone4s
 #THEOS_DEVICE_IP = 192.168.1.6 #ipad
 #THEOS_DEVICE_IP = 192.168.1.11 #ipod
+THEOS_DEVICE_IP = 129.21.138.139 #rit iphone6+
 
 TARGET := iphone:8.4:2.0
 ARCHS := armv7 arm64
--- slicespreferences/SlicesPreferencesListController.mm
+++ slicespreferences/SlicesPreferencesListController.mm
@@ -2,6 +2,8 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 
+#import <Social/Social.h>
+
 #import <AppList/AppList.h>
 
 #import "../Model/GameCenterAccountManager.h"
@@ -32,6 +34,42 @@
 	}
 
 	return _specifiers;
+}
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+	// get bundle
+	NSBundle *bundle = [NSBundle bundleForClass:[SlicesPreferencesListController class]];
+
+	// add heart image
+	UIImage *image = [UIImage imageNamed:@"heart" inBundle:bundle compatibleWithTraitCollection:nil];
+
+	if (image)
+	{
+		UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStyleBordered target:self action:@selector(heartTapped:)];
+		barButtonItem.tintColor = [UIColor redColor];
+		self.navigationItem.rightBarButtonItem = barButtonItem;
+	}
+}
+
+- (void)heartTapped:(id)sender
+{
+	SLComposeViewController *composeViewController;
+	if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+		composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+	else if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook])
+		composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+	else if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeSinaWeibo])
+		composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeSinaWeibo];
+	else if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTencentWeibo])
+		composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTencentWeibo];
+
+	[composeViewController setInitialText:@"I use #Slices to manage multiple settings, accounts, and data bundles for apps!"];
+	[composeViewController addURL:[NSURL URLWithString:@"http://cydia.saurik.com/package/org.thebigboss.slices/"]];
+	
+	[self presentViewController:composeViewController animated:YES completion:nil];
 }
 
 - (void)openTwitter:(id)arg1
